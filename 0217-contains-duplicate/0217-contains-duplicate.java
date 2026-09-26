379f68a74873c5e2c9a2e9c2e6ff5063b86bff2d
Saransh Dhiman <saranshdhiman353@gmail.com>
2026-09-26 17:10:46 +0530

    Time: 26 ms (9.91%), Space: 81.3 MB (96.69%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int index =0;
+        while(index < nums.length-1){
+            if(nums[index] == nums[index+1]) return true;
+            else index++;
+        }
+        return false;
+    }
+}
